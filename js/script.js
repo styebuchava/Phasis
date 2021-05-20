12e620e459f9d3dcaad8b57fb02d2480ce26707c
Saba Tkebuchava <81930150+styebuchava@users.noreply.github.com>
2021-05-20 12:37:31 +0400

    add burger bar menu and responsive for header

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var menbuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
+
+sideNav.style.right = "-250px";
+
+menbuBtn.onclick = function(){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+        menu.src = 'images/close.png'; 
+    }
+    else{
+        sideNav.style.right = "-250px";
+        menu.src = 'images/menu.png'; 
+    }
+}
